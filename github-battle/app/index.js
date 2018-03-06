df9ab6b742f25f65a97216e3488910ff7d63b4cc
viktharien <viktharien@zoho.com>
2018-03-05 20:44:32 -0600

    work on tab component

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -3,30 +3,16 @@ var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 require('./index.css');
 
-class Badge extends React.Component {
+class App extends React.Component {
     render() {
         return(
             <div>
-                <img
-                    src={this.props.img}
-                    alt="Avatar"
-                    style={{width: 100, height: 100}}
-                />
-                <h1>Name: {this.props.name}</h1>
-                <h3>Username: {this.props.username}</h3>
+                
             </div>
         )
     }
 }
 
-Badge.propTypes = {
-    img: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired
-}
 
-ReactDOM.render(<Badge
-        img="http://via.placeholder.com/150x150.png"
-        name="viktharien"
-        username="viktharien"
+ReactDOM.render(<App
     />, document.getElementById("app"));
