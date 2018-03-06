bad196013c292383b7e5c50ea72479b97e07edd3
viktharien <viktharien@zoho.com>
2018-03-05 20:45:24 -0600

    work on tab component

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -2,16 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 require('./index.css');
+var App = require('./components/App')
 
-class App extends React.Component {
-    render() {
-        return(
-            <div>
-               Hello World! 
-            </div>
-        )
-    }
-}
 
 
 ReactDOM.render(<App
