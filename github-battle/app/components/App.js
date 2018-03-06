7a323dc9a93b16788fb3fb8c5aefb3e75b41a5a5
viktharien <viktharien@zoho.com>
2018-03-05 20:53:06 -0600

    work on tab component

--- github-battle/app/components/App.js
+++ github-battle/app/components/App.js
@@ -3,7 +3,7 @@ var Popular = require('./Popular');
 class App extends React.Component {
     render() {
         return(
-            <div>
+            <div className="container">
                 <Popular />
             </div>
         );
