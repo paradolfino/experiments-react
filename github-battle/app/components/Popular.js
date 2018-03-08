40920a8dc2f40e8b640aeefbd33b145fdc062ea8
viktharien <viktharien@zoho.com>
2018-03-07 20:55:54 -0600

    popular component

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -22,7 +22,7 @@ class Popular extends React.Component {
                     {languages.map((lang)=>{
                         return (
                         <li 
-                            style={lang === this.state.selectedLanguage ? { color: 'red' }}
+                            style={lang === this.state.selectedLanguage ? { color: 'red' } : { color: 'black' }}
                             key={lang} 
                             onClick={this.updateLanguage.bind(null, lang)}>
                             {lang}
