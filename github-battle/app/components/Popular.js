2fe21d6724b2a052a001adab79a8fdef99e2b351
viktharien <viktharien@zoho.com>
2018-03-07 20:39:53 -0600

    popular component

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -3,8 +3,7 @@ var React = require('react');
 class Popular extends React.Component {
     render() {
         var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-        return 
-            (<ul>
+        return (<ul>
                 {languages.map(function(item, index){
                     return <li key={index}>{item}</li>;
                 })}
