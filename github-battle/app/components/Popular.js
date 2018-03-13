99cbb14d54f31d8a8caa90ac2a9fe84756b6e761
viktharien <viktharien@zoho.com>
2018-03-12 20:38:30 -0500

    stateless functional components

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,5 +1,22 @@
 var React = require('react');
 
+class SelectLanguage extends React.Component {
+    render() {
+        return (<ul className="languages">
+                    {languages.map((lang)=>{
+                        return (
+                        <li 
+                            style={lang === this.state.selectedLanguage ? { color: 'red' } : null}
+                            key={lang} 
+                            onClick={this.updateLanguage.bind(null, lang)}>
+                            {lang}
+                        </li>
+                    );
+                    })}
+                </ul>);
+    }
+}
+
 class Popular extends React.Component {
     constructor() {
         super();
@@ -18,18 +35,7 @@ class Popular extends React.Component {
     }
     render() {
         var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-        return (<ul className="languages">
-                    {languages.map((lang)=>{
-                        return (
-                        <li 
-                            style={lang === this.state.selectedLanguage ? { color: 'red' } : null}
-                            key={lang} 
-                            onClick={this.updateLanguage.bind(null, lang)}>
-                            {lang}
-                        </li>
-                    );
-                    })}
-                </ul>);
+        return null;
     }
 }
 
