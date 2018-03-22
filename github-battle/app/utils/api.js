2ee5b3f32904af989ebeec0edf05064722aa7f1e
viktharien <viktharien@zoho.com>
2018-03-21 20:53:41 -0500

    implement axios

--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -3,5 +3,8 @@ var axios = require('axios');
 module.exports = {
     fetchPopularRepos: function (lang) {
         var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + lang + '&sort=stars&order=desc&type=Repositories');
+        return axios.get(encodeURI).then(function() {
+
+        });
     }
 };
